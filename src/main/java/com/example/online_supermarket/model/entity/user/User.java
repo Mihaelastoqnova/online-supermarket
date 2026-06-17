6f07ed9fd6d0eb6427f6c1b6eb521fba8c384a88
Mihaelastoqnova <mihaela.stoqnova@abv.bg>
2026-06-17 15:04:09 +0300

    Create User entity

---
+++ src/main/java/com/example/online_supermarket/model/entity/user/User.java
@@ -0,0 +1,62 @@
+package com.example.online_supermarket.model.entity.user;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@Entity
+@Table
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50)
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotBlank(message = "First name is required")
+    @Column(nullable = false)
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String profilePicture;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private LoyaltyCard loyaltyCard;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Order> orders;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Basket basket;
+}
+
---
+++ src/main/java/com/example/online_supermarket/model/entity/user/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.online_supermarket.model.entity.user;
+
+public enum UserRole {
+    ADMIN, USER;
+}
